bc17520ed4267b224f01eaf764647913b507a6a0
Jose Hernandez <josehdzibarra@hotmail.com>
2024-02-10 14:32:54 -0600

    Implemented meanCamberLine and thicknessDistribution

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ https://aerospaceweb.org/question/airfoils/q0041.shtml
 import os
 import matplotlib.pyplot as plt
 import argparse
+import math
 
 # plt.plot([1, 2, 3, 4])
 # plt.ylabel('some numbers')
@@ -45,12 +46,16 @@ def meanCamberLine(m:int,p:int,t:int,jump:int=0.01):
         yc.append(m*((1-2*p)+2*p*i-i**2)/(1-p)**2)
 
     return tuple(yc)
+
+
 def thicknessDistribution(t:int,jump:int = 0.01):
     '''
     Function to get the thickness distribution
     :param jump:
+    :type jump: int
     :raise:
     :return: A tuple with the values yt from 0 to 1 in x
+    :rtype: tuple
     '''
     c = 1
     t=c*(t/100)
@@ -58,9 +63,31 @@ def thicknessDistribution(t:int,jump:int = 0.01):
     yt =[]
     # From x = 0 to x = c
     for i in range(0,c+1,jump):
-        
         yt.append(t*(0.2969*(i)**(1/2)-0.126*i-0.3516*i**2+0.2843*i**3-0.1015*i**4)/0.2)
 
+    return tuple(yt)
     
-def fun3():
-    ...
+def thetaValues(jump:int = 0.01):
+    '''
+    Function to get the angle in each point of the camber line
+
+    :param jump:
+    :type jump: int 
+    :return: A tuple with the values of theta from 0 to 1 in x
+    :rtype: tuple
+    '''
+
+    # Theta = arctan(d(yc)/dx)
+    # yc has a function from x = 0 to x = p
+    theta = []
+    for x in range(0,p,jump):
+        dyc = 2*m*(p-x)/p**2
+        theta.append(math.atan(dyc))
+        
+    # yc has a function from x = p to x = c and c = 1
+    for x in range(p,c,jump):
+        dyc = 2*m*(p-x)/(1-p)**2
+        theta.append(math.atan(dyc))
+    
+    return tuple(theta)
+
